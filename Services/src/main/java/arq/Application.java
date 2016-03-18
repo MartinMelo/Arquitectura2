abd4f37f59291fd7c9b6e352df8c0bcb36afe8b7
marianovarela <marianogvarela@gmail.com>
2016-03-18 18:27:56 -0300

    logging con slf4j

--- Services/src/main/java/arq/Application.java
+++ Services/src/main/java/arq/Application.java
@@ -1,5 +1,7 @@
 package arq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +12,7 @@ import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import arq.config.RepositoryRestConfiguration;
+import arq.controller.ExceptionController;
 
 @Configuration
 @EnableJpaRepositories
@@ -19,8 +22,11 @@ import arq.config.RepositoryRestConfiguration;
 @EnableHypermediaSupport(type = { EnableHypermediaSupport.HypermediaType.HAL })
 @ComponentScan
 public class Application {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+    	LOGGER.info("arrancooooooo");
         SpringApplication.run(Application.class, args);
     }
     
