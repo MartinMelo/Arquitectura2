aef3b0ca54fe9895ceeb804a909d2179a6bd3962
marianovarela <marianogvarela@gmail.com>
2016-04-24 23:39:55 -0300

    fix en la creacion de las entidades

--- src/main/java/arq/controller/ShopController.java
+++ src/main/java/arq/controller/ShopController.java
@@ -1,14 +1,19 @@
 package arq.controller;
 
-import arq.domain.Shop;
-import arq.repository.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import arq.domain.Shop;
+import arq.repository.ShopRepository;
 
 /**
  * Created by Martin Alejandro on 4/11/2016.
@@ -20,13 +25,14 @@ public class ShopController {
 
     @Autowired
     ShopRepository shopRepository;
-
+    
     @RequestMapping(method = RequestMethod.POST)
-    public Shop save(@RequestParam("name") String name, @RequestParam("name") String location) {
-        Shop shop = new Shop();
-        shop.setName(name);
-        shop.setLocation(location);
-        return shopRepository.save(shop);
+    public ResponseEntity<Shop> save(@RequestBody Shop shop, UriComponentsBuilder builder) {
+        Shop aShop = shopRepository.save(shop);
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/shops/{id}").buildAndExpand(aShop.getId()).toUri());
+        return new ResponseEntity<Shop>(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
